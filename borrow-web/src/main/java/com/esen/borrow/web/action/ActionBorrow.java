5add4b9673219ce1570099113de3e48d0398104f
liuaj <liuaj@esensoft.com>
2022-08-19 16:12:51 +0800

    CHG:[图书管理]添加查看图书列表、根据类型查看对应的图书列表、新建图书、、记录查询、新建借阅等功能

--- borrow-web/src/main/java/com/esen/borrow/web/action/ActionBorrow.java
+++ borrow-web/src/main/java/com/esen/borrow/web/action/ActionBorrow.java
@@ -1,6 +1,10 @@
 package com.esen.borrow.web.action;
 
+import com.esen.book.api.entity.BookCategoryEntity;
+import com.esen.book.api.entity.BookHistoryEntity;
+import com.esen.book.api.entity.BookTypeEntity;
 import com.esen.book.api.entity.BookViewEntity;
+import com.esen.borrow.api.Repository.BorrowViewRepository;
 import com.esen.borrow.api.entity.BorrowViewEntity;
 import com.esen.borrow.web.service.BorrowService;
 import com.esen.ejdbc.params.PageRequest;
@@ -11,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
 import java.util.List;
-
 
 /**
  * 书籍管理类，负责借阅的增删改查
@@ -23,44 +27,114 @@ import java.util.List;
 @Controller
 @RequestMapping("/study/borrow")
 public class ActionBorrow {
-		/**
-		 * 对于这个变量的描述
-		 */
-		@Autowired
-		private BorrowService borrowService;
+	private static final String REPEAT = "书名重复";
 
+	/**
+	 * 对于这个变量的描述
+	 */
+	@Autowired
+	private BorrowService borrowService;
 
-		/**
-		 * 对于这个方法的描述
-		 * @param
+	@RequestMapping("test")
+	public String test(){
+		return "borrow/addBook";
+	}
+
+	//------------------------------------------------借阅功能----------------------------------------------------------------
+	/**
+	 * 获取借阅列表
+	 */
+	@RequestMapping(value = "/borrowList")
+	public String borrowList(HttpServletRequest req) {
+		PageRequest page = new PageRequest(1, 10);
+		List<BorrowViewEntity> list = borrowService.borrowList(page);
+		req.setAttribute("borrowlist", list);
+		return "borrow/borrowList";
+	}
+	/**
+	 *  添加借书记录
+	 *  @param person 借阅人 bid 书籍ID fromdate 借书时间
+	 * 	@return 添加结果
+	 */
+	@RequestMapping(value = "/addBorrow", method = RequestMethod.POST)
+	@ResponseBody
+	public String addBorrow(String person, String bid, Timestamp fromdate) {
+		borrowService.addBorrow(person, bid, fromdate);
+		return "借书成功！";
+	}
+	/**
+	 * 更新借书记录，还书
+	 */
+	@RequestMapping(value = "/saveBorrow",method = RequestMethod.POST)
+	public String saveBorrow(BorrowViewEntity borrowViewEntity){
+		borrowService.returnBook(borrowViewEntity);
+		return "还书成功";
+	}
+
+	public String deleteBorrow(BorrowViewEntity borrowViewEntity){
+		if (borrowService.deleteBorrow(borrowViewEntity)){
+			return "删除成功！";
+		}
+		return  "删除失败";
+	}
+
+	//------------------------------------------------图书功能----------------------------------------------------------------
+	/**
+	 * 获取小类列表，可得到全部列表或根据大类获取对应的小类列表
+	 * @param cid 大类ID
+	 * @return
+	 */
+	@RequestMapping(value = "/typeList")
+	@ResponseBody
+	public List<BookTypeEntity> typeList(String cid) {
+		return borrowService.typeList(cid);
+
+	}
+	/**
+	 * 获取大类列表
+	 * @param
+	 * @return
+	 * @throws
+	 */
+	@RequestMapping(value = "/categoryList")
+	@ResponseBody
+	public List<BookCategoryEntity> categoryList() {
+		return borrowService.categoryList();
+	}
+
+	/**
+		 * 获取图书列表，可得到全部列表或根据大类获取对应的图书列表
+		 * @param bcaption bcaption(可为空) 图书大类
 		 * @return
-		 * @throws
 		 */
-		@RequestMapping(value = "/test2")
-		public String getTotalCountByPerson2(HttpServletRequest req, String person){
-				req.setAttribute("number", borrowService.getTotalCountByPerson("张三"));
-				return "borrow/test";
-		}
+	@RequestMapping(value = "/bookList")
+	public String bookList(HttpServletRequest req, String bcaption) {
+		PageRequest page = new PageRequest(1, 10);
+		List<BookViewEntity> list = borrowService.bookList(page, bcaption);
+		req.setAttribute("booklist", list);
+		return "borrow/bookList";
+	}
 
-		@RequestMapping(value = "/test3")
-		public String test3(HttpServletRequest req){
-				return "borrow/index";
+	/**
+	 * 添加书籍
+	 *  @param name 书籍名 scaptionn 小类名称 desc 书本描述
+	 * 	@return 添加结果
+	 */
+	@RequestMapping(value = "/addBook", method = RequestMethod.POST)
+	@ResponseBody
+	public String addBook(String name, String tid, String desc) {
+		if (borrowService.bookIsExists(name) == REPEAT) {
+			return "书名重复!";
 		}
-
-		@RequestMapping(value = "/list1")
-		public String borrowList(HttpServletRequest req){
-				PageRequest page = new PageRequest(1,10);
-				List<BorrowViewEntity> list = borrowService.borrowList(page);
-				req.setAttribute("borrowlist",list);
-				return "borrow/list";
-		}
-
-		@RequestMapping(value = "/list2")
-		public String bookList(HttpServletRequest req,String bcaption){
-			PageRequest page = new PageRequest(1,10);
-			List<BookViewEntity> list = borrowService.bookList(page,bcaption);
-			req.setAttribute("booklist",list);
-			return "borrow/booklist";
-		}
+		borrowService.addBook(name, tid, desc);
+		return "添加成功！";
+	}
+	/**
+	 * 编辑书籍
+	 */
+	@RequestMapping(value = "/saveBook",method = RequestMethod.POST)
+	public String saveBook(String name,String desc,String scaption,String bcaption){
+		return null;
+	}
 
 }
--- borrow-web/src/main/java/com/esen/borrow/web/service/BorrowService.java
+++ borrow-web/src/main/java/com/esen/borrow/web/service/BorrowService.java
@@ -1,8 +1,7 @@
 package com.esen.borrow.web.service;
 
-import com.esen.book.api.entity.BookViewEntity;
-import com.esen.book.api.repository.BookHistoryRepository;
-import com.esen.book.api.repository.BookViewRepository;
+import com.esen.book.api.entity.*;
+import com.esen.book.api.repository.*;
 import com.esen.borrow.api.Repository.BorrowViewRepository;
 import com.esen.borrow.api.entity.BorrowViewEntity;
 import com.esen.ejdbc.jdbc.ConnectFactoryManager;
@@ -10,16 +9,18 @@ import com.esen.ejdbc.params.PageRequest;
 import com.esen.ejdbc.params.PageResult;
 import com.esen.eorm.annotation.ApplicationService;
 import com.esen.eorm.service.AbstractService;
+import com.esen.eutil.util.UNID;
 import com.esen.eutil.util.exp.Expression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /**
- * 借阅的SERVICE层
+ * 图书管理的SERVICE层
  *
  * @author liuaj
  * @since 20220816
@@ -35,6 +36,19 @@ public class BorrowService extends AbstractService<BorrowViewEntity> {
 
 	@Autowired
 	protected BookViewRepository bookViewRepository;
+
+	@Autowired
+	protected BookInfoRepository bookInfoRepository;
+
+	@Autowired
+	protected BookTypeRepository bookTypeRepository;
+
+	@Autowired
+	protected BookCategoryRepository bookCategoryRepository;
+
+	@Autowired
+	protected BookHistoryRepository bookHistoryRepository;
+
 //------------------------------------------------借阅功能----------------------------------------------------------------
 	/**
 	 * person的所有借阅记录数
@@ -54,12 +68,62 @@ public class BorrowService extends AbstractService<BorrowViewEntity> {
 		PageResult<BorrowViewEntity>result = borrowViewRepository.findAll(page);
 		return result.list();
 	}
-//------------------------------------------------图书功能----------------------------------------------------------------
 	/**
-	 * 获取图书列表
-	 * @param page 页面信息 bcaption(可为空) 图书大类
-	 * @return List<BookViewEntity> 图书列表
+	 * 添加借书记录
+	 * @param person 借阅人 bid 书籍ID fromdate 借书时间
+	 * @return
+	 */
+	public void addBorrow(String person, String bid, Timestamp fromdate){
+		String id = UNID.randomID();
+		BookHistoryEntity bookHistoryEntity = new BookHistoryEntity();
+		bookHistoryEntity.setId(id);
+		bookHistoryEntity.setBid(bid);
+		bookHistoryEntity.setPerson(person);
+		bookHistoryEntity.setFromdate(fromdate);
+
+	}
+	public boolean deleteBorrow(BorrowViewEntity borrowViewEntity){
+		return bookHistoryRepository.remove(borrowViewEntity.getId());
+	}
+
+	/**
+	 * 更新借书记录，还书
+	 * @param
+	 * @return
 	 * @throws
+	 */
+	public void returnBook(BorrowViewEntity borrowViewEntity){
+		String id = borrowViewEntity.getId();
+		BookHistoryEntity bookHistoryEntity=
+				(BookHistoryEntity) bookHistoryRepository.findOneQuietly(new Expression("id=?"),new Object[]{id});
+		bookHistoryEntity.setTodate(borrowViewEntity.getTodate());
+		bookHistoryRepository.save(bookHistoryEntity,"todate");
+	}
+//------------------------------------------------图书功能----------------------------------------------------------------
+
+	/**
+	 * 获取大类列表
+	 */
+	public List<BookCategoryEntity> categoryList(){
+
+		return (List<BookCategoryEntity>)bookCategoryRepository.findAll();
+	}
+	/**
+	 * 获取小类列表，可得到全部列表或根据大类获取对应的小类列表
+	 * @param cid 大类ID
+	 * @return
+	 */
+	public List<BookTypeEntity> typeList(String cid){
+		if (cid!=null){
+			return (List<BookTypeEntity>)bookTypeRepository.findAll(new Expression("cid=?"),new Object[]{cid});
+		}
+		return (List<BookTypeEntity>) bookTypeRepository.findAll();
+
+	}
+	/**
+	 * 获取图书列表，可得到全部列表或根据大类获取对应的图书列表
+	 * @param page 页面信息 bcaption(可为空) 图书大类
+	 * @return
 	 */
 	public List<BookViewEntity> bookList(PageRequest page,String bcaption){
 		PageResult<BookViewEntity> result = bookViewRepository.findAll(page);
@@ -70,6 +134,32 @@ public class BorrowService extends AbstractService<BorrowViewEntity> {
 		}
 		return result.list();
 	}
-
+	/**
+	 * 检查书本是否存在
+	 * @param name 书本名
+	 * @return
+	 */
+	public  String bookIsExists(String name){
+		BookInfoEntity bookInfoEntity =
+				bookInfoRepository.findOneQuietly(new Expression("caption=?"),new Object[]{name});
+		if (bookInfoEntity!=null){
+			return "书名重复";
+		}
+		return null;
+	}
+	/**
+	 * 添加书籍
+	 * @param name 书籍名 scaptionn 小类名称 desc 书本描述
+	 * @return
+	 */
+	public void addBook( String name,String tid,String desc){
+		String id = UNID.randomID();
+		BookInfoEntity bookInfoEntity = new BookInfoEntity();
+		bookInfoEntity.setId(id);
+		bookInfoEntity.setTid(tid);
+		bookInfoEntity.setCaption(name);
+		bookInfoEntity.setDesc(desc);
+		bookInfoRepository.add(bookInfoEntity);
+	}
 
 }
